d04801df0d65aab76df3518989cfc1021e8031b6
Aiwei Zheng <Aiwei Zheng>
2017-04-14 20:00:26 -0400

    refactor

--- app/albums/album.routing.ts
+++
@@ -1,6 +0,0 @@
-import {RouterModule,Router} from "@angular/router";
-import{AlbumsComponent} from "./albums.component";
-
-export const albumRouting = RouterModule.forChild([
-    { path: 'albums', component: AlbumsComponent }
-]);
